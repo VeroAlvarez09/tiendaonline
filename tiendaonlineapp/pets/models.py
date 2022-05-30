e317ee200a1f76a873ae1f9778d9c3c78c1b7909
Veronica Alvarez Saldarriaga <veronica.alvarez562@pascualbravo.edu.co>
2022-05-30 11:19:36 -0500

    Creando el modelo producto

--- tiendaonlineapp/pets/models.py
+++ tiendaonlineapp/pets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
+    value = models.FloatField(default=0)
+    image = models.CharField(max_length=500)
+    active = models.BooleanField(default=1)
