058e12600bc26e36f6ee1e3b9e924731c1db12c0
Veronica Alvarez Saldarriaga <veronica.alvarez562@pascualbravo.edu.co>
2022-05-30 12:48:42 -0500

    Imagen y vista de productos 4 huellitas

--- tiendaonlineapp/pets/views.py
+++ tiendaonlineapp/pets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+def index(request):
+    products_list = Product.objects.filter(quantity=1).filter(active = 1)
+    return render(request,"pets/index.html",{
+        "products_list" : products_list
+    })
